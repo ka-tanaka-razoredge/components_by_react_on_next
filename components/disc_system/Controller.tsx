960a885eb6dd2c4c3ba87098f6b319727bfe3f5b
ka-tanaka-razoredge <software@razor-edge.net>
2022-11-24 09:15:09 +0900

    Controller get handle Tab

--- components/disc_system/Controller.tsx
+++ components/disc_system/Controller.tsx
@@ -41,13 +41,37 @@ export default forwardRef((props, ref) => {
 //    params.append('json', encodeURIComponent(json));
     await axios.post(`https://razor-edge.net/cakephp-2.4.4/sailing_ships/save`, params);
   };
+
+  function onTabKey( e, obj ){
+    obj = textarea.current;
   
+  	// タブキーが押された時以外は即リターン
+  	if( e.keyCode!=9 ){ return; }
+  
+  	// タブキーを押したときのデフォルトの挙動を止める
+  	e.preventDefault();
+  
+  	// 現在のカーソルの位置と、カーソルの左右の文字列を取得しておく
+  	var cursorPosition = obj.selectionStart;
+  	var cursorLeft     = obj.value.substr( 0, cursorPosition );
+  	var cursorRight    = obj.value.substr( cursorPosition, obj.value.length );
+  
+  	// テキストエリアの中身を、
+  	// 「取得しておいたカーソルの左側」+「タブ」+「取得しておいたカーソルの右側」
+  	// という状態にする。
+  	obj.value = cursorLeft+"\t"+cursorRight;
+  
+  	// カーソルの位置を入力したタブの後ろにする
+  	obj.selectionEnd = cursorPosition+1;
+  }  
   
   return (
     <div>
+      <style>
+      </style>
       <div className={ props.classes }>
         JSON:<br />
-        <textarea ref={ textarea } cols='1000' rows='1000' style={{ width: '1000px', height: '250px' }} onChange={ (e) => { setJson(e.target.value);  } } value={ json }></textarea><br />
+        <textarea ref={ textarea } cols='1000' rows='1000' style={{ width: '1000px', height: '250px', tabSize: '2' }} onChange={ (e) => { setJson(e.target.value);  } } onKeyDown={ (e) => { onTabKey(e); } } value={ json }></textarea><br />
         <input type='button' value='apply' onClick={ (e) => { apply(); } } />
         <input type='button' value='fetch' onClick={ (e) => { fetch(); } } /><br />
         id:&nbsp;<input type='text' onChange={ (e) => { setId(e.target.value); } } value={ id } />
