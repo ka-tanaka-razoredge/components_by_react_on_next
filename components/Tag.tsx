ce7dda0327a031ccc28bb4e44936ba83f4204d5d
root <root@v157-7-196-174.myvps.jp>
2024-12-27 07:08:24 +0900

    components直下の*.(tsx||ts)を削除

--- components/Tag.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    <div style={{ padding: '1px 1px 1px 1px', color: 'white', backgroundColor: 'pink' }}>
-      { props.value }
-    </div>
-  )
-}
--- components/Variable.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    <span>
-      <font color='blue'>{ props.value }</font>:&nbsp;
-    </span>
-  )
-}
